ffd8234d3c59ae73824671b0adb7aa5ba8bfdf76
Damien Soulard <damien.soulard@gmail.com>
2017-09-12 11:54:24 -0400

    :chart_with_upwards_trend: setup Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Damien Soulard`,
   },
-  plugins: [`gatsby-plugin-glamor`,],
+  plugins: [
+    `gatsby-plugin-glamor`,
+    {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: 'UA-106325824-1',
+    },
+  },
+  ],
 }
