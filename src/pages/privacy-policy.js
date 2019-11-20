337cbcf5b69e84e7d254bb42f8fcc0d434e76b1d
Damien Soulard <damien.soulard@gmail.com>
2019-11-20 17:33:07 +0100

    feat: add terms of use page

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import Paragraph from "../components/Paragraph";
 import Section from "../components/Section";
 import Title from "../components/Title";
 
---
+++ src/pages/terms-of-use.js
@@ -0,0 +1,296 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import Section from "../components/Section";
+import Title from "../components/Title";
+
+export default props => (
+  <Layout location={props.location}>
+    <Section htmlID={""}>
+      <h1>Terms of use</h1>
+      <Title anchor="intro">1. TERMS OF USE</Title>
+      <p>
+        By downloading, browsing, accessing or using our applications, you agree
+        to be bound by these Terms and Conditions of Use. We reserve the right
+        to amend these terms and conditions at any time. If you disagree with
+        any of these Terms and Conditions of Use, you must immediately
+        discontinue your access to our applications and your use of the services
+        offered on them. Continued use of them will constitute acceptance of
+        these Terms and Conditions of Use, as may be amended from time to time.
+      </p>
+      <Title anchor="ip">2. DEFINITIONS</Title>
+      <p>
+        In these Terms and Conditions of Use, the following capitalised terms
+        shall have the following meanings, except where the context otherwise
+        requires: "Account" means an account created by a User on our
+        applications as part of Registration. "Merchant" refers to any entity
+        whose products or Samples can be purchased and/or redeemed (as the case
+        may be) via our applications. "Privacy Policy" means the privacy policy
+        set out in Clause 14 of these Terms and Conditions of Use. "Redeem"
+        means to redeem a Merchant’s products or Samples on these Terms and
+        Conditions of Use and "Redemption" means the act of redeeming such
+        products or Samples. "Register" means to create an Account on our
+        applications and "Registration" means the act of creating such an
+        Account. "Samples" means samples of Merchant’s products and/or
+        Merchant’s services, including without limitation, any vouchers for the
+        redemption of such services, and "Sample" means any one of them.
+        "Services" means all the services provided by our applications to Users,
+        and "Service" means any one of them, "Users" means users of our
+        applications, including you and "User" means any one of them.
+      </p>
+      <Title anchor="access">
+        3. GENERAL ISSUES ABOUT OUR APPLICATIONS AND THE SERVICES
+      </Title>
+      <p>
+        (A) Applicability of terms and conditions: The use of any Services
+        and/or our applications and the making of any Redemptions are subject to
+        these Terms and Conditions of Use.
+      </p>
+      <p>
+        (B) Prevention on use: We reserve the right to prevent you using our
+        applications and the Service (or any part of them) and to prevent you
+        from making any Redemptions.
+      </p>
+      <p>
+        (C) Equipment and Networks: The provision of the Services and our
+        applications does not include the provision of a mobile telephone or
+        handheld device or other necessary equipment to access our applications
+        or the Services or make any Redemptions. To use our applications or
+        Services or to make Redemptions, you will require Internet connectivity
+        and appropriate telecommunication links. You acknowledge that the terms
+        of agreement with your respective mobile network provider ("Mobile
+        Provider") will continue to apply when using the Mobile Application. As
+        a result, you may be charged by the Mobile Provider for access to
+        network connection services for the duration of the connection while
+        accessing the Mobile Application or any such third party charges as may
+        arise. You accept responsibility for any such charges that arise.
+      </p>
+      <p>
+        (D) Permission to use Mobile Application: If you are not the bill payer
+        for the mobile telephone or handheld device being used to access the
+        Mobile Application, you will be assumed to have received permission from
+        the bill payer for using the Mobile Application.
+      </p>
+      <p>
+        (E) License to Use Material: By submitting any text or images (including
+        photographs) (“Material”) via the Application, you represent that you
+        are the owner of the Material, or have proper authorization from the
+        owner of the Material to use, reproduce and distribute it. You hereby
+        grant us a worldwide, royalty-free, non-exclusive license to use the
+        Material to promote any products or services.
+      </p>
+
+      <Title anchor="registration">4. REDEMPTIONS</Title>
+      <p>
+        (A) Need for registration: You must Register to make a Redemption from
+        the Mobile Application.
+      </p>
+      <p>
+        (B) Redemption: Any attempted Redemption not consistent with these Terms
+        and Conditions of Use may be disallowed or rendered void at our or the
+        relevant Merchant’s discretion.
+      </p>
+
+      <Title anchor="content">5. NOTIFICATIONS</Title>
+      <p>
+        You agree to receive pre-programmed notifications on the Mobile
+        Application if you have turned on locational services on your mobile
+        telephone or other handheld devices (as the case may be).
+      </p>
+      <Title anchor="third-party">6. YOUR OBLIGATIONS</Title>
+      <p>
+        (A) Merchant terms: You agree to (and shall) abide by the terms and
+        conditions of the relevant Merchant for which your Redemption relates
+        to, as may be amended from time to time.
+      </p>
+      <p>
+        (B) Accurate information: You warrant that all information provided on
+        Registration and contained as part of your Account is true, complete and
+        accurate and that you will promptly inform us of any changes to such
+        information by updating the information in your Account.
+      </p>
+      <p>
+        (C) Content on the Mobile Application and Service: It is your
+        responsibility to ensure that any products, Samples or information
+        available through the Mobile Application or the Services meet your
+        specific requirements before making any Redemption.
+      </p>
+      <Title anchor="third-party">
+        7. RULES ABOUT USE OF THE SERVICE AND OUR APPLICATIONS
+      </Title>
+      <p>
+        (A) We will use reasonable endeavours to correct any errors or omissions
+        as soon as practicable after being notified of them. However, we do not
+        guarantee that the Services or the Mobile Application will be free of
+        faults, and we do not accept liability for any such faults, errors or
+        omissions. In the event of any such error, fault or omission, you should
+        report it by contacting us at damien.soulard@gmail.com.
+      </p>
+      <p>
+        (B) We do not warrant that your use of the Services or the Mobile
+        Application will be uninterrupted and we do not warrant that any
+        information (or messages) transmitted via the Services or the Mobile
+        Application will be transmitted accurately, reliably, in a timely manner
+        or at all. Notwithstanding that we will try to allow uninterrupted
+        access to the Services and the Mobile Application, access to the
+        Services and the Mobile Application may be suspended, restricted or
+        terminated at any time.
+      </p>
+      <p>
+        (C) We reserve the right to change, modify, substitute, suspend or
+        remove without notice any information or Services on the Mobile
+        Application from time to time. Your access to the Mobile Application
+        and/or the Services may also be occasionally restricted to allow for
+        repairs, maintenance or the introduction of new facilities or services.
+        We will attempt to restore such access as soon as we reasonably can. For
+        the avoidance of doubt, we reserve the right to withdraw any information
+        or Services from the Mobile Application at any time.
+      </p>
+      <p>
+        (D) We reserve the right to block access to and/or to edit or remove any
+        material which in our reasonable opinion may give rise to a breach of
+        these Terms and Conditions of Use.
+      </p>
+      <Title anchor="warranties">8. SUSPENSION AND TERMINATION</Title>
+      <p>
+        (A) If you use (or anyone other than you, with your permission uses) the
+        Mobile Application, any Services in contravention of these Terms and
+        Conditions of Use, we may suspend your use of the Services and/or Mobile
+        Application.
+      </p>
+      <p>
+        (B) If we suspend the Services or Mobile Application, we may refuse to
+        restore the Services or Mobile Application for your use until we receive
+        an assurance from you, in a form we deem acceptable, that there will be
+        no further breach of the provisions of these Terms and Conditions of
+        Use.
+      </p>
+      <p>
+        (C) Without limitation to anything else in this Clause 8, we shall be
+        entitled immediately or at any time (in whole or in part) to: (a)
+        suspend the Services and/or Mobile Application; (b) suspend your use of
+        the Services and/or Mobile Application; and/or (c) suspend the use of
+        the Services and/or Mobile Application for persons we believe to be
+        connected (in whatever manner) to you, if: 8.C.1 you commit any breach
+        of these Terms and Conditions of Use; 8.C.2 we suspect, on reasonable
+        grounds, that you have, might or will commit a breach of these Terms and
+        Conditions of Use; or 8.C.3 we suspect, on reasonable grounds, that you
+        may have committed or be committing any fraud against us or any person.
+      </p>
+      <p>
+        (D) Our rights under this Clause 8 shall not prejudice any other right
+        or remedy we may have in respect of any breach or any rights,
+        obligations or liabilities accrued prior to termination.
+      </p>
+      <Title anchor="termination">
+        10. DISCLAIMER AND EXCLUSION OF LIABILITY
+      </Title>
+      <p>
+        (A) The Mobile Application, the Services, the information on the Mobile
+        Application and use of all related facilities are provided on an "as is,
+        as available" basis without any warranties whether express or implied.
+      </p>
+      <p>
+        (B) To the fullest extent permitted by applicable law, we disclaim all
+        representations and warranties relating to the Mobile Application and
+        its contents, including in relation to any inaccuracies or omissions in
+        the Mobile Application, warranties of merchantability, quality, fitness
+        for a particular purpose, accuracy, availability, non-infringement or
+        implied warranties from course of dealing or usage of trade.
+      </p>
+      <p>
+        (C) We do not warrant that the Mobile Application will always be
+        accessible, uninterrupted, timely, secure, error free or free from
+        computer virus or other invasive or damaging code or that the Mobile
+        Application will not be affected by any acts of God or other force
+        majeure events, including inability to obtain or shortage of necessary
+        materials, equipment facilities, power or telecommunications, lack of
+        telecommunications equipment or facilities and failure of information
+        technology or telecommunications equipment or facilities.
+      </p>
+      <p>
+        (D) While we may use reasonable efforts to include accurate and
+        up-to-date information on the Mobile Application, we make no warranties
+        or representations as to its accuracy, timeliness or completeness.
+      </p>
+      <p>
+        (E) We shall not be liable for any acts or omissions of any third
+        parties howsoever caused, and for any direct, indirect, incidental,
+        special, consequential or punitive damages, howsoever caused, resulting
+        from or in connection with the mobile application and the services
+        offered in the mobile application, your access to, use of or inability
+        to use the mobile application or the services offered in the mobile
+        application, reliance on or downloading from the mobile application
+        and/or services, or any delays, inaccuracies in the information or in
+        its transmission including but not limited to damages for loss of
+        business or profits, use, data or other intangible, even if we have been
+        advised of the possibility of such damages.
+      </p>
+      <p>
+        (F) We shall not be liable in contract, tort (including negligence or
+        breach of statutory duty) or otherwise howsoever and whatever the cause
+        thereof, for any indirect, consequential, collateral, special or
+        incidental loss or damage suffered or incurred by you in connection with
+        the Mobile Application and these Terms and Conditions of Use. For the
+        purposes of these Terms and Conditions of Use, indirect or consequential
+        loss or damage includes, without limitation, loss of revenue, profits,
+        anticipated savings or business, loss of data or goodwill, loss of use
+        or value of any equipment including software, claims of third parties,
+        and all associated and incidental costs and expenses.
+      </p>
+      <p>
+        (G) The above exclusions and limitations apply only to the extent
+        permitted by law. None of your statutory rights as a consumer that
+        cannot be excluded or limited are affected.
+      </p>
+      <p>
+        (H) Notwithstanding our efforts to ensure that our system is secure, you
+        acknowledge that all electronic data transfers are potentially
+        susceptible to interception by others. We cannot, and do not, warrant
+        that data transfers pursuant to the Mobile Application, or electronic
+        mail transmitted to and from us, will not be monitored or read by
+        others.
+      </p>
+      <Title anchor="copyright">11. INTELLECTUAL PROPERTY RIGHTS</Title>
+      <p>
+        (A) All editorial content, information, photographs, illustrations,
+        artwork and other graphic materials, and names, logos and trade marks on
+        the Mobile Application are protected by copyright laws and/or other laws
+        and/or international treaties, and belong to us and/or our suppliers, as
+        the case may be. These works, logos, graphics, sounds or images may not
+        be copied, reproduced, retransmitted, distributed, disseminated, sold,
+        published, broadcasted or circulated whether in whole or in part, unless
+        expressly permitted by us and/or our suppliers, as the case may be.
+      </p>
+      <p>
+        (B) Nothing contained on the Mobile Application should be construed as
+        granting by implication, estoppel, or otherwise, any license or right to
+        use any trademark displayed on the Mobile Application without our
+        written permission. Misuse of any trademarks or any other content
+        displayed on the Mobile Application is prohibited.
+      </p>
+      <p>
+        (C) We will not hesitate to take legal action against any unauthorised
+        usage of our trade marks, name or symbols to preserve and protect its
+        rights in the matter. All rights not expressly granted herein are
+        reserved. Other product and company names mentioned herein may also be
+        the trade marks of their respective owners.
+      </p>
+      <Title anchor="law">12. AMENDMENTS</Title>
+      <p>
+        (A) We may periodically make changes to the contents of the Mobile
+        Application, including to the descriptions and prices of goods and
+        services advertised, at any time and without notice. We assume no
+        liability or responsibility for any errors or omissions in the content
+        of the Mobile Application.
+      </p>
+      <p>
+        (B) We reserve the right to amend these Terms and Conditions of Use from
+        time to time without notice. The revised Terms and Conditions of Use
+        will be posted on the Mobile Application and shall take effect from the
+        date of such posting. You are advised to review these terms and
+        conditions periodically as they are binding upon you.
+      </p>
+    </Section>
+  </Layout>
+);
